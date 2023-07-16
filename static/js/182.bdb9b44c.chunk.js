3d449dab198be86da28b8c48ddf7bce04ad03477
vinayakkurugovinakoppa <vinayakkurugovinakoppa@qnap.com>
2023-07-16 14:19:28 +0530

    Updates

---
+++ static/js/182.bdb9b44c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhosting=self.webpackChunkhosting||[]).push([[182],{182:function(e,t,i){i.r(t);i(791);var u=i(184);t.default=function(){return(0,u.jsx)("div",{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, exercitationem accusamus saepe itaque consequuntur nostrum, officiis tempora vitae repellendus non culpa quam delectus amet accusantium voluptates perferendis fugiat dolorem. Obcaecati."})}}}]);
+//# sourceMappingURL=182.bdb9b44c.chunk.js.map
